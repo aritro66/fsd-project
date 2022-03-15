e8362d4ecd2a614b22516c53ed42bcce0ab39b67
aritro66 <ghosharitro66@gmail.com>
2022-03-15 10:34:35 +0530

    adding landing page, login page and sign up page

---
+++ public/signuplogin.js
@@ -0,0 +1,64 @@
+var pass=document.getElementById("passicon");
+var repass=document.getElementById("repassicon");
+var inputpass=document.getElementById("pass");
+var inputrepass=document.getElementById("repass");
+
+pass.addEventListener("click",()=>{
+    if(pass.classList.contains("fa-eye"))
+    {
+        pass.classList.add("fa-eye-slash");
+        pass.classList.remove("fa-eye");
+        inputpass.setAttribute("type","text");
+    }
+    else
+    {
+        pass.classList.add("fa-eye");
+        pass.classList.remove("fa-eye-slash");
+        inputpass.setAttribute("type","password");
+    }
+});
+
+inputpass.addEventListener("focus",()=>{
+    if(pass.classList.contains("fa-eye"))
+    {
+        document.getElementById("eyeband").style.display="block";
+    }
+    else
+    {
+        document.getElementById("eyeband").style.display="none";
+    }
+});
+
+inputpass.addEventListener("blur",()=>{
+    document.getElementById("eyeband").style.display="none";
+});
+
+repass.addEventListener("click",()=>{
+    if(repass.classList.contains("fa-eye"))
+    {
+        repass.classList.add("fa-eye-slash");
+        repass.classList.remove("fa-eye");
+        inputrepass.setAttribute("type","text");
+    }
+    else
+    {
+        repass.classList.add("fa-eye");
+        repass.classList.remove("fa-eye-slash");
+        inputrepass.setAttribute("type","password");
+    }
+});
+
+inputrepass.addEventListener("focus",()=>{
+    if(repass.classList.contains("fa-eye"))
+    {
+        document.getElementById("eyeband").style.display="block";
+    }
+    else
+    {
+        document.getElementById("eyeband").style.display="none";
+    }
+});
+
+inputrepass.addEventListener("blur",()=>{
+    document.getElementById("eyeband").style.display="none";
+});
